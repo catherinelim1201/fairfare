f5bdf4b3cdd7fdf49f90ba49a69f033acd72b62a
catherine lim <jkvvhappy@gmail.com>
2023-09-19 17:43:56 +0800

    currently attempt to display splits for each contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -20,4 +20,36 @@ class ContactsController < ApplicationController
       end
     end
   end
+
+  def index
+    # user = User.requests_for(current_user)
+    @split_ids = current_user.split_ids
+
+    @splits = []
+
+    @split_ids.each do |split_id|
+      @split = Split.find(split_id)
+      @splits << @split
+
+      @splitmember = SplitMember.find_by(split_id: split_id)
+      @member = Member.find(@splitmember.member_id)
+      @contact = Contact.find(@member.id)
+      raise
+
+    end
+
+    # @split_members = []
+    # @split_member_ids = @split.split_member.id
+    # @split_member_ids.each do |split_member_id|
+    #   @split_members << @split_member_id
+    # end
+    # @splits.user_id = User.requests_for(current_user)
+
+    # @split.user_id = Split.find(params[:split_id])
+    # @split.user = current_user
+
+    # @contacts = Contact.all
+    # @split_members = SplitMember.all
+    # @splits = Split.all
+  end
 end
